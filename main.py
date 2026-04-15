6af0926fbcc4f3ac2c819af9304d0d809e0f3539
cakezhcn <1828229252@qq.com>
2026-04-15 19:18:17 +0800

    Create main.py file with AdapterManager class and main entry point for QQ Official Bot Adapter

---
+++ main.py
@@ -0,0 +1,10 @@
+class AdapterManager:
+    def __init__(self):
+        pass
+
+    def start(self):
+        print("Starting QQ Official Bot Adapter...")
+
+if __name__ == '__main__':
+    manager = AdapterManager()
+    manager.start()
